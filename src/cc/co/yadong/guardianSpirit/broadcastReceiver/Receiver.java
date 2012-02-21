2513ac1bdd7469e17e15f0ff680df85496a98c8a
yadong.huang <huangyadong@live.cn>
2012-02-21 23:09:11 +0800

    add a receiver

---
+++ src/cc/co/yadong/guardianSpirit/broadcastReceiver/Receiver.java
@@ -0,0 +1,18 @@
+package cc.co.yadong.guardianSpirit.broadcastReceiver;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Receiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if("".equals(action)){
+			
+		}else if("".equals(action)){
+			
+		}
+	}
+
+}
