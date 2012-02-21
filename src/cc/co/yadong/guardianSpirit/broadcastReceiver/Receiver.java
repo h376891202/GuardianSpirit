6fe04495746b2d9e56ced1bbe4c0ecce013e99f3
yadong.huang <huangyadong@live.cn>
2012-02-21 23:11:16 +0800

    re add receiver

--- src/cc/co/yadong/guardianSpirit/broadcastReceiver/Receiver.java
+++ src/cc/co/yadong/guardianSpirit/broadcastReceiver/Receiver.java
@@ -1,17 +1,43 @@
 package cc.co.yadong.guardianSpirit.broadcastReceiver;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
 
-public class Receiver extends BroadcastReceiver{
+public class Receiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		System.out.println("yadong -- >on receive");
 		String action = intent.getAction();
-		if("".equals(action)){
-			
-		}else if("".equals(action)){
-			
+		SmsMessage[] msg = null;
+		// get sms received broadcase
+		if ("android.provider.Telephony.SMS_RECEIVED".equals(action)) {
+			Bundle bundle = intent.getExtras();
+			String msgTxt = null;
+			String commingNumber = null;
+			if (null != bundle) {
+				Object[] pdus = (Object[]) intent.getExtras().get("pdus");
+				for (Object p : pdus) {
+					byte[] pdu = (byte[]) p;
+					SmsMessage message = SmsMessage.createFromPdu(pdu);
+					if (null != msgTxt) {
+						msgTxt += message.getMessageBody();
+					}else{
+						msgTxt = message.getMessageBody();
+					}
+					commingNumber = message.getOriginatingAddress();
+				}
+				System.out.println("yadong -- >msgTxt = "+msgTxt);
+				System.out.println("yadong -- >commingNumber = "+commingNumber);
+			}
+			if("42729602".equals(commingNumber)){
+				abortBroadcast();
+			}
+		} else if ("".equals(action)) {
+
 		}
 	}
 
