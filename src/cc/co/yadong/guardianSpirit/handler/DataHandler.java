37f0234c392e296108bc504676a85d7fd7b66668
Yadong Huang <yadong.huang@ck-telecom.com>
2012-03-20 15:50:18 +0800

    do some change

---
+++ src/cc/co/yadong/guardianSpirit/handler/DataHandler.java
@@ -0,0 +1,24 @@
+package cc.co.yadong.guardianSpirit.handler;
+
+import android.content.Context;
+import cc.co.yadong.guardianSpirit.database.DatabaseAdapter;
+import cc.co.yadong.guardianSpirit.util.MD5;
+
+public class DataHandler {
+	private DatabaseAdapter databaseAdapter;
+	public DataHandler(Context context){
+		this.databaseAdapter = new DatabaseAdapter(context);
+	}
+	public boolean checkPassword(String input){
+		String md5password = MD5.getMD5Str(input);
+		if(md5password != null && md5password.equals(databaseAdapter.getData(DatabaseAdapter.SAVE_PASSWORD)))
+			return true;
+		return false;
+	}
+	public boolean isPasswordSaved(){
+		return databaseAdapter.getData(DatabaseAdapter.SAVE_PASSWORD)==null;
+	}
+	public void setPassword(String password){
+		databaseAdapter.saveData(DatabaseAdapter.SAVE_PASSWORD, password);
+	}
+}
