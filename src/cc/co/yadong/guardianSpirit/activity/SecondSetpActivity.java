5c165ad3aff0c527e06e382507a6c3e4d2a71b95
Yadong Huang <yadong.huang@ck-telecom.com>
2012-04-05 20:41:36 +0800

    add a activity

---
+++ src/cc/co/yadong/guardianSpirit/activity/SecondSetpActivity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cc.co.yadong.guardianSpirit.activity;
+
+import cc.co.yadong.guardianSpirit.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author admin
+ *
+ */
+public class SecondSetpActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_setup);
+	}
+
+}
--- src/cc/co/yadong/guardianSpirit/activity/Welcom.java
+++ src/cc/co/yadong/guardianSpirit/activity/Welcom.java
@@ -4,19 +4,44 @@
 package cc.co.yadong.guardianSpirit.activity;
 
 import cc.co.yadong.guardianSpirit.R;
+import cc.co.yadong.guardianSpirit.handler.DataHandler;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * @author admin
  *
  */
-public class Welcom extends Activity{
-
+public class Welcom extends Activity implements OnClickListener{
+	private DataHandler dataHandler = null;
+	private Button mCancelButton;
+	private Button mNextButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		dataHandler = new DataHandler(this);
+		if(dataHandler.isPasswordSaved()){
+			//TODO
+			this.finish();
+		}
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcom);
+		mCancelButton = (Button) findViewById(R.id.cancel_button);
+		mNextButton = (Button) findViewById(R.id.ok_button);
+		mCancelButton.setOnClickListener(this);
+		mNextButton.setOnClickListener(this);
+	}
+	public void onClick(View v) {
+		if(v.equals(mCancelButton)){
+			
+		}else{
+			Intent intent = new Intent(Welcom.this,SecondSetpActivity.class);
+			startActivity(intent);
+		}
+		
 	}
 	
 
